c5f2b6fb47d125aa81b7888a5a322a27ac3e9048
mweinberger-tgm <mweinberger@student.tgm.ac.at>
2015-01-11 13:50:14 +0100

    hier wird versucht die argumente einzulesen

---
+++ src/tgm/exp/testing/options.java
@@ -0,0 +1,12 @@
+package tgm.exp.testing;
+
+/**
+ * 
+ * Ein Versuch mit der Apache Commons CLI Argumente zu interpretieren.
+ * 
+ * @author Michael Weinberger 4AHITT
+ *
+ */
+public class options {
+	
+}
