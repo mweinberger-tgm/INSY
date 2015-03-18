4b0b3e412bd46c56b7ce51449a3c428c536e03d3
mweinberger-tgm <mweinberger@student.tgm.ac.at>
2015-03-18 22:09:03 +0100

    Almost done

--- files/create.sql
+++ files/create.sql
@@ -1,21 +1,5 @@
 --Michael Weinberger 4AHIT, erstellt am 18.03.2015
-
-CREATE TABLE Boot(
-	tiefgang DECIMAL,
-	personen SMALLINT,
-	name VARCHAR(100),
-	id INTEGER PRIMARY KEY
-);
-
-CREATE TABLE Tourenboot(
-	bootsklasse	VARCHAR(50),
-	id INTEGER PRIMARY KEY REFERENCES Boot(id)
-);
-
-CREATE TABLE Sportboot(
-	segelflaeche DECIMAL, 
-	id INTEGER PRIMARY KEY REFERENCES Boot(id)
-);
+--modelliert nach Vorgabe des RM
 
 --Serial ist unter Postgres Auto Increment!
 CREATE TABLE Person(
@@ -24,16 +8,80 @@ CREATE TABLE Person(
 	geburtsdatum DATE
 );
 
-CREATE TABLE Trainer(
-		
-);
-
 CREATE TABLE Segler(
 	key SERIAL REFERENCES Person(key) PRIMARY KEY	
 );
 
-CREATE TABLE zugewiesen( 
-	id INTEGER REFERENCES Boot(id),
-	name VARCHAR(100) REFERENCES Mannschaft(name),
+CREATE TABLE Trainer(
+	key SERIAL REFERENCES Person(key) PRIMARY KEY	
+);
+
+--/////
+
+CREATE TABLE Boot(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(100),
+	personen SMALLINT,
+	tiefgang DECIMAL	
+);
+
+--Ist 'id INTEGER PRIMARY KEY REFERENCES Boot(id)' auch valide?
+CREATE TABLE Tourenboot(
+	id SERIAL REFERENCES Boot(id) PRIMARY KEY,
+	bootsklasse	VARCHAR(50)
+);
+
+CREATE TABLE Sportboot(
+	id SERIAL REFERENCES Boot(id) PRIMARY KEY ,
+	segelflaeche DECIMAL
+);
+
+--/////
+
+--Altersklasse beinhaltet nicht nur Nummern, sonder bspw. auch Buchstaben wie z.B. U20
+CREATE TABLE Mannschaft(
+	name VARCHAR (50) PRIMARY KEY,
+	aklasse VARCHAR(20),
+	key SERIAL REFERENCES Trainer(key)
+);
+
+--/////
+
+--land im Laendercode, z.B. AT, DE, CH
+CREATE TABLE Regatta(
+	name VARCHAR(50),
+	jahr SMALLINT,
+	land VARCHAR(2), 
+	PRIMARY KEY(name, jahr)
+);
+
+CREATE TABLE Wettfahrt(
+	name VARCHAR(50) REFERENCES Regatta(name),
+	jahr SMALLINT REFERENCES Regatta(jahr),
+	datum DATE,
+	laenge DECIMAL,
+	PRIMARY KEY (name, jahr, datum)
+);
+
+--/////
+
+CREATE TABLE bildet(
+	key SERIAL REFERENCES Segler(key),
+	name VARCHAR(50) REFERENCES Mannschaft(name),
+	PRIMARY KEY(key, name)
+);
+
+CREATE TABLE zugewiesen(
+	id SERIAL REFERENCES Boot(id),
+	name VARCHAR(50) REFERENCES Mannschaft(name),
 	PRIMARY KEY(id, name)
+);
+
+CREATE TABLE nimmt_teil(
+	mname VARCHAR(50) REFERENCES Mannschaft(name),
+	rname VARCHAR(50) REFERENCES Regatta(name)
+);
+
+CREATE TABLE erzielt(
+		
 );
