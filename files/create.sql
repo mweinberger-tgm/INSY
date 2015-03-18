6c17a32dfbd09454ef61e2867646b4ee6c64aed0
mweinberger-tgm <mweinberger@student.tgm.ac.at>
2015-03-18 21:12:47 +0100

    Erste Fortschritte

--- files/create.sql
+++ files/create.sql
@@ -6,3 +6,23 @@ CREATE TABLE Boot(
 	name VARCHAR(100),
 	id INTEGER PRIMARY KEY
 );
+
+CREATE TABLE Tourenboot(
+	bootsklasse	VARCHAR(50),
+	id INTEGER PRIMARY KEY REFERENCES Boot(id)
+);
+
+CREATE TABLE Sportboot(
+	segelflaeche DECIMAL, 
+	id INTEGER PRIMARY KEY REFERENCES Boot(id)
+);
+
+CREATE TABLE Person(
+	key
+);
+
+CREATE TABLE zugewiesen( 
+	id INTEGER REFERENCES Boot(id),
+	name VARCHAR(50) REFERENCES Mannschaft(name),
+	PRIMARY KEY(id, name)
+);
