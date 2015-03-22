94cba7ce7b8e948edc6feb9ed90dd2cebe47a3e8
mweinberger-tgm <mweinberger@student.tgm.ac.at>
2015-03-22 13:42:17 +0100

    Weiterer Fortschritt.

---
+++ files/insert.sql
@@ -0,0 +1,15 @@
+--Michael Weinberger 4AHIT, erstellt am 22.03.2015
+
+\i content_Person.sql;
+\i content_Segler.sql;
+\i content_Trainer.sql;
+\i content_Boot.sql;
+\i content_Tourenboot.sql;
+\i content_Sportboot.sql;
+\i content_Mannschaft.sql;
+\i content_Regatta.sql;
+\i content_Wettfahrt.sql;
+\i content_bildet.sql;
+\i content_zugewiesen.sql;
+\i content_nimmt_teil.sql;
+\i content_erzielt.sql;
--- files/start.sql
+++ files/start.sql
@@ -2,4 +2,4 @@
 
 \i drop.sql
 \i create.sql
---\i insert.sql
+\i insert.sql
